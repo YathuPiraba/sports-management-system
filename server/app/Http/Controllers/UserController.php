1445fcec8f4d71210a1667d5e026da231a780166
YathuPiraba <yathavan1407@gmail.com>
2024-07-31 16:32:17 +0530

    login api successful

---
+++ server/app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Log in a user.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'userName' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        // Retrieve user
+        $user = User::where('userName', $request->userName)->first();
+
+        if ($user && Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Login successful',
+                'user' => $user,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Invalid login credentials'
+            ], 401);
+        }
+    }
+
+
+}
