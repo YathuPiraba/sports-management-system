28f6ac1316e8501a230cc33385c95d786fd99268
YathuPiraba <yathavan1407@gmail.com>
2024-08-02 17:26:40 +0530

    tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,36 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import daisyui from "daisyui";
 
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: '"Poppins", sans-serif',
+        roboto: '"Roboto", sans-serif',
+        acme: '"Acme", sans-serif',
+      },
+      colors: {
+        primary: "#232E42",
+        primaryDefault: "#1959AA",
+        primaryRed: "#E62F55",
+      },
+    },
+  },
+  plugins: [
+    daisyui,
+    function ({ addUtilities }) {
+      const newUtilties = {
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none",
+        },
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+      };
+
+      addUtilities(newUtilties);
+    },
+  ],
+};
