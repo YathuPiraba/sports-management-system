540b845b9694f1bd9375809f62081d7dd73f9769
YathuPiraba <yathavan1407@gmail.com>
2024-08-09 09:06:01 +0530

    secured api

--- client/src/pages/login/config.js
+++ client/src/pages/login/config.js
@@ -2,19 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup,FacebookAuthProvider , GoogleAuthProvider } from "firebase/auth";
 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAzHDUJ05MzY4XlI7vE2iWZk9NjkgYgeG8",
-  authDomain: "sports-management-system-d189d.firebaseapp.com",
-  projectId: "sports-management-system-d189d",
-  storageBucket: "sports-management-system-d189d.appspot.com",
-  messagingSenderId: "581948476686",
-  appId: "1:581948476686:web:b14bac7207df087927db5f",
-  measurementId: "G-5SHPJS2EWJ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const fbAuthProvider = new FacebookAuthProvider()
