b8e44059e0fc3b0c3ec357b07d02590d17c20a90
YathuPiraba <yathavan1407@gmail.com>
2024-08-16 11:05:27 +0530

    forgot password fixed

--- client/src/pages/login/ForgotPassword.jsx
+++ client/src/pages/login/ForgotPassword.jsx
@@ -11,8 +11,8 @@ import {
 const ForgotPassword = ({ isVisible, onClose }) => {
   const [step, setStep] = useState(1);
   const [email, setEmail] = useState("");
-  const [otp, setOtp] = useState(null);
-  const [timer, setTimer] = useState(600); 
+  const [otp, setOtp] = useState("");
+  const [timer, setTimer] = useState(600);
   const [isTimerRunning, setIsTimerRunning] = useState(false);
 
   const {
@@ -45,6 +45,7 @@ const ForgotPassword = ({ isVisible, onClose }) => {
       await forgotPasswordAPI({ email: email });
       toast.success("OTP sent to your email.");
       setEmail(email);
+      reset();
       setStep(2);
       startTimer();
     } catch (error) {
@@ -55,13 +56,15 @@ const ForgotPassword = ({ isVisible, onClose }) => {
 
   const verifyOTP = async (data) => {
     try {
-      const formData = new FormData();
-      formData.append("otp", data.otp);
-      formData.append("email", email);
+      const payload = {
+        email: email,
+        otp: data.otp,
+      };
 
-      await verifyOtpAPI(formData);
-      setOtp(data.otp)
+      await verifyOtpAPI(payload);
+      setOtp(data.otp);
       toast.success("OTP verified successfully.");
+      reset();
       setStep(3);
     } catch (error) {
       toast.error("Invalid OTP. Please try again.");
@@ -70,12 +73,16 @@ const ForgotPassword = ({ isVisible, onClose }) => {
 
   const resetPassword = async (data) => {
     try {
-      const formData = new FormData();
-      formData.append('email', email);
-      formData.append('otp', otp);
-      formData.append('password', data.password);
-  
-      await resetPasswordAPI(formData);
+      const payload = {
+        email: email,
+        otp: otp,
+        password: data.password,
+        password_confirmation: data.confirm_password,
+      };
+
+      console.log(otp);
+
+      await resetPasswordAPI(payload);
       toast.success("Password reset successfully.");
       reset();
       onClose();
@@ -90,28 +97,46 @@ const ForgotPassword = ({ isVisible, onClose }) => {
         return (
           <form
             onSubmit={handleSubmit((data) => sendOTP(data.email))}
-            autocomplete="off"
+            className="space-y-2"
           >
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email:
+            </label>
             <Controller
               name="email"
               control={control}
               render={({ field }) => (
                 <Input
                   {...field}
+                  id="email"
                   placeholder="Enter your email"
-                  style={{ marginBottom: "1rem" }}
+                  className="w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
                   autoComplete="off"
                 />
               )}
             />
-            <Button type="primary" htmlType="submit" block>
+            <Button
+              type="primary"
+              htmlType="submit"
+              block
+              className="w-full bg-blue-500 text-white py-2 rounded-md"
+            >
               Send OTP
             </Button>
           </form>
         );
       case 2:
         return (
-          <form onSubmit={handleSubmit(verifyOTP)}>
+          <form onSubmit={handleSubmit(verifyOTP)} className="space-y-2">
+            <label
+              htmlFor="otp"
+              className="block text-sm font-medium text-gray-700"
+            >
+              OTP:
+            </label>
             <Controller
               name="otp"
               control={control}
@@ -119,34 +144,46 @@ const ForgotPassword = ({ isVisible, onClose }) => {
               render={({ field }) => (
                 <Input
                   {...field}
+                  id="otp"
                   placeholder="Enter OTP"
-                  style={{ marginBottom: "1rem" }}
+                  className="w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
                 />
               )}
             />
             {errors.otp && (
-              <p style={{ color: "red", marginBottom: "1rem" }}>
-                {errors.otp.message}
-              </p>
+              <p className="text-red-600 text-sm mb-2">{errors.otp.message}</p>
             )}
-            <p>
+            <p className="text-gray-600">
               Time remaining: {Math.floor(timer / 60)}:
               {timer % 60 < 10 ? "0" : ""}
               {timer % 60}
             </p>
-            <Space>
-              <Button type="primary" htmlType="submit">
+            <div className="flex space-x-2">
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="w-full bg-blue-500 text-white py-2 rounded-md"
+              >
                 Verify OTP
               </Button>
-              <Button onClick={() => sendOTP(email)} disabled={isTimerRunning}>
+              <Button
+                onClick={() => sendOTP(email)}
+                className="w-full bg-gray-500 text-white py-2 rounded-md"
+              >
                 Send OTP Again
               </Button>
-            </Space>
+            </div>
           </form>
         );
       case 3:
         return (
-          <form onSubmit={handleSubmit(resetPassword)}>
+          <form onSubmit={handleSubmit(resetPassword)} className="space-y-2">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              New Password:
+            </label>
             <Controller
               name="password"
               control={control}
@@ -154,17 +191,42 @@ const ForgotPassword = ({ isVisible, onClose }) => {
               render={({ field }) => (
                 <Input.Password
                   {...field}
+                  id="password"
                   placeholder="Enter new password"
-                  style={{ marginBottom: "1rem" }}
+                  className="w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                />
+              )}
+            />
+            <label
+              htmlFor="confirm_password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Confirm Password:
+            </label>
+            <Controller
+              name="confirm_password"
+              control={control}
+              rules={{ required: "Confirm password is required" }}
+              render={({ field }) => (
+                <Input.Password
+                  {...field}
+                  id="confirm_password"
+                  placeholder="Confirm your new password"
+                  className="w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
                 />
               )}
             />
             {errors.password && (
-              <p style={{ color: "red", marginBottom: "1rem" }}>
+              <p className="text-red-600 text-sm mb-2">
                 {errors.password.message}
               </p>
             )}
-            <Button type="primary" htmlType="submit" block>
+            <Button
+              type="primary"
+              htmlType="submit"
+              block
+              className="w-full bg-blue-500 text-white py-2 rounded-md"
+            >
               Reset Password
             </Button>
           </form>
