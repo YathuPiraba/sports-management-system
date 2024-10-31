108d13628779c410b199cd6837951fadd6d51a20
YathuPiraba <yathavan1407@gmail.com>
2024-10-31 10:16:10 +0530

    changed layout

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -148,131 +148,136 @@ const Login = () => {
   };
 
   return (
-    <Suspense fallback={<div >Loading...</div>}>
-      <div className="body-container">
-        <div className="outter relative">
-          
-          <div className="login">
-            <div className="topic">
-              <div className="logo">
-                <img
-                  src="https://res.cloudinary.com/dmonsn0ga/image/upload/v1723798132/logo2_qanauk.png"
-                  alt="Club Connect Logo"
-                />
-                <h1>
-                  <span className="title">C</span>
-                  <span className="title3">lub </span>
-                  <span className="title1">C</span>
-                  <span className="title3">onnect</span>
-                </h1>
-              </div>
-              <div className="moto">
-              <p>Unite Every Club, Connect Every Player...!</p>
-              </div>
-            </div>
-            <div className="loginform">
-              <form onSubmit={handleFormSubmit} className="space-y-2 lg:space-y-4 ">
-                <input
-                  type="text"
-                  name="userName"
-                  placeholder="Enter your username"
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
-                  {...register("userName", { required: true })}
-                />
-                {errors.username && <span>Username is required</span>}
-                <Controller
-                  name="password"
-                  control={control}
-                  rules={{ required: true }}
-                  render={({ field }) => (
-                    <Input.Password
-                      {...field}
-                      placeholder="Enter your password"
-                      visibilityToggle={{
-                        visible: passwordVisible,
-                        onVisibleChange: setPasswordVisible,
-                      }}
-                      className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
-                    />
-                  )}
-                />
-                {errors.password && <span>Password is required</span>}
-                <div className="flex justify-between gap-1 items-center">
-                  <span
-                    onClick={showForgotPasswordModal}
-                    className="forgot-btn cursor-pointer hover:underline"
-                  >
-                    Forgot Password?
-                  </span>
-                  <Button
-                    htmlType="submit"
-                    className="px-6 text-white py-2 rounded-md bg-transparent loginbtn"
-                    loading={loading}
-                  >
-                    Login
-                  </Button>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="body-container flex">
+        <div className=" min-h-screen sep"></div>
+        <div className=" min-h-screen cont">
+          <div className="outter">
+            <div className="login">
+              <div className="topic">
+                <div className="logo">
+                  <img
+                    src="https://res.cloudinary.com/dmonsn0ga/image/upload/v1723798132/logo2_qanauk.png"
+                    alt="Club Connect Logo"
+                  />
+                  <h1>
+                    <span className="title">C</span>
+                    <span className="title3">lub </span>
+                    <span className="title1">C</span>
+                    <span className="title3">onnect</span>
+                  </h1>
                 </div>
-              </form>
-              <ForgotPassword
-                isVisible={isForgotPasswordModalVisible}
-                onClose={handleForgotPasswordCancel}
-              />
-              <div className="mt-4 text-center">
-                <p className="text-md font-roboto ac ">
-                  Don't have an account?{" "}
-                  <a
-                    href="#"
-                    className="text-blue-400 hover:underline text-md ml-3"
-                    onClick={showModal}
-                  >
-                    Sign up
-                  </a>
-                </p>
+                <div className="moto">
+                  <p>Unite Every Club, Connect Every Player...!</p>
+                </div>
+              </div>
+              <div className="loginform w-4/5">
+                <form
+                  onSubmit={handleFormSubmit}
+                  className="space-y-2 lg:space-y-4 "
+                >
+                  <input
+                    type="text"
+                    name="userName"
+                    placeholder="Enter your username"
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+                    {...register("userName", { required: true })}
+                  />
+                  {errors.username && <span>Username is required</span>}
+                  <Controller
+                    name="password"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => (
+                      <Input.Password
+                        {...field}
+                        placeholder="Enter your password"
+                        visibilityToggle={{
+                          visible: passwordVisible,
+                          onVisibleChange: setPasswordVisible,
+                        }}
+                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+                      />
+                    )}
+                  />
+                  {errors.password && <span>Password is required</span>}
+                  <div className="flex justify-between gap-1 items-center">
+                    <span
+                      onClick={showForgotPasswordModal}
+                      className="forgot-btn cursor-pointer hover:underline"
+                    >
+                      Forgot Password?
+                    </span>
+                    <Button
+                      htmlType="submit"
+                      className="px-6 text-white py-2 rounded-md bg-transparent loginbtn"
+                      loading={loading}
+                    >
+                      Login
+                    </Button>
+                  </div>
+                </form>
+                <ForgotPassword
+                  isVisible={isForgotPasswordModalVisible}
+                  onClose={handleForgotPasswordCancel}
+                />
+                <div className="mt-4 text-center">
+                  <p className="text-md font-roboto ac ">
+                    Don't have an account?{" "}
+                    <a
+                      href="#"
+                      className="text-blue-400 hover:underline text-md ml-3"
+                      onClick={showModal}
+                    >
+                      Sign up
+                    </a>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="SignUp">
-          <Modal
-            title={<div style={{ textAlign: "center" }}>Sign Up</div>}
-            open={isModalVisible}
-            onCancel={handleCancel}
-            footer={null}
-            width={400}
-          >
-            <div className="signup-options flex justify-center">
-              {clubLoading ? (
-                <div>
-                  <PropagateLoader color="blue" />
-                </div>
-              ) : (
-                <>
-                  <Button
-                    onClick={() => navigate("/signup/manager")}
-                    type="primary"
-                    block
-                    style={{ width: "150px", margin: "10px auto 0" }}
-                  >
-                    Manager
-                  </Button>
-                  <Button
-                    onClick={() => navigate("/signup/member")}
-                    type="primary"
-                    block
-                    style={{ width: "150px", margin: "10px auto 0" }}
-                    disabled={clubs?.length === 0}
-                    title={
-                      clubs?.length === 0
-                        ? "Can't register since no managers are registered"
-                        : ""
-                    }
-                  >
-                    Member
-                  </Button>
-                </>
-              )}
-            </div>
-          </Modal>
+          <div className="SignUp">
+            <Modal
+              title={<div style={{ textAlign: "center" }}>Sign Up</div>}
+              open={isModalVisible}
+              onCancel={handleCancel}
+              footer={null}
+              width={400}
+            >
+              <div className="signup-options flex justify-center">
+                {clubLoading ? (
+                  <div>
+                    <PropagateLoader color="blue" />
+                  </div>
+                ) : (
+                  <>
+                    <Button
+                      onClick={() => navigate("/signup/manager")}
+                      type="primary"
+                      block
+                      style={{ width: "150px", margin: "10px auto 0" }}
+                    >
+                      Manager
+                    </Button>
+                    <Button
+                      onClick={() => navigate("/signup/member")}
+                      type="primary"
+                      block
+                      style={{ width: "150px", margin: "10px auto 0" }}
+                      disabled={clubs?.length === 0}
+                      title={
+                        clubs?.length === 0
+                          ? "Can't register since no managers are registered"
+                          : ""
+                      }
+                    >
+                      Member
+                    </Button>
+                  </>
+                )}
+              </div>
+            </Modal>
+          </div>
         </div>
       </div>
     </Suspense>
